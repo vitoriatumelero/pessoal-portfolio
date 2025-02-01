30bd978aee61d4469795e223ceb48154611d0c53
Vitória Tumelero <vitoriatumelero1807@gmail.com>
2025-02-01 17:03:51 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />
     </div>
   );
 }
